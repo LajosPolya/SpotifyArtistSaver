1c21e99dbcd4025cb9d7819839ec3941d3203320
LajosPolya <lajospolya94@gmail.com>
2020-02-02 01:45:52 -0500

    Pull artist and artists

--- src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
+++ src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
@@ -1,11 +1,15 @@
 package com.lajospolya.spotifyscraper;
 
-import com.lajospolya.spotifyapiwrapper.authorization.SpotifyAuthorizationManager;
+import com.lajospolya.spotifyapiwrapper.authorization.AuthorizationResponse;
 import com.lajospolya.spotifyapiwrapper.client.SpotifyApiClient;
+import com.lajospolya.spotifyapiwrapper.client.response.Artist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
@@ -17,8 +21,19 @@ public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
     public void run(ApplicationArguments args)
     {
 
-        SpotifyApiClient client = SpotifyAuthorizationManager.getAuthorizedApiClient(clientAuthorizationProperties.getClientId(), clientAuthorizationProperties.getClientSecret());
-        client.getArtist();
+        //BQCuC86c2cQHXuNtQrdy6ranMewVMZOQ2DBWev-gQppLmAuC7Ze5nKBMVqPhkJHolexf3jbVapkiW8Zf4CY
+        AuthorizationResponse authorizationResponse = new AuthorizationResponse();
+        authorizationResponse.setAccessToken("BQCuC86c2cQHXuNtQrdy6ranMewVMZOQ2DBWev-gQppLmAuC7Ze5nKBMVqPhkJHolexf3jbVapkiW8Zf4CY");
+        authorizationResponse.setTokenType("Bearer");
+        SpotifyApiClient client = new SpotifyApiClient(authorizationResponse);
+        //SpotifyApiClient client = SpotifyAuthorizationManager.getAuthorizedApiClient(clientAuthorizationProperties.getClientId(), clientAuthorizationProperties.getClientSecret());
+        Artist arcticMonkeys = client.getArtist("7Ln80lUS6He07XvHI8qqHH");
+
+        List<String> artistIds = new ArrayList<>();
+        artistIds.add("4LLpKhyESsyAXpc4laK94U");
+        artistIds.add("7Ln80lUS6He07XvHI8qqHH");
+
+        List<Artist> artists = client.getArtists(artistIds);
 
         System.out.println("App Started");
         System.out.println(clientAuthorizationProperties.getClientId());
