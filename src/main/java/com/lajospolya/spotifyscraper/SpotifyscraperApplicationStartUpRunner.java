afc652860f3db7031d0dcaa8192485401f88d3ff
LajosPolya <lajospolya94@gmail.com>
2020-02-02 15:41:53 -0500

    Artist's top tracks

--- src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
+++ src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
@@ -4,6 +4,7 @@ import com.lajospolya.spotifyapiwrapper.authorization.AuthorizationResponse;
 import com.lajospolya.spotifyapiwrapper.client.SpotifyApiClient;
 import com.lajospolya.spotifyapiwrapper.client.response.Artist;
 import com.lajospolya.spotifyapiwrapper.client.response.SimplifiedAlbum;
+import com.lajospolya.spotifyapiwrapper.client.response.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -22,7 +23,7 @@ public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
     public void run(ApplicationArguments args)
     {
         AuthorizationResponse authorizationResponse = new AuthorizationResponse();
-        authorizationResponse.setAccessToken("BQBLRW_lvFMqpOZGXVVpeeOAtbqsWwKFBW-9EhfeShbAEbbyCeUZSnrFZw70qHuZxuNQ7aXsn4zmN1f5Puk");
+        authorizationResponse.setAccessToken("BQBT991MQaMP-EL9cK24q6JNdtDC0tFGfDcH6wy6BqxPGhoXaQx3wHcCJClfaRZnnWJdDXSrS9-S-bxcYEY");
         authorizationResponse.setTokenType("Bearer");
         SpotifyApiClient client = new SpotifyApiClient(authorizationResponse);
         //SpotifyApiClient client = SpotifyAuthorizationManager.getAuthorizedApiClient(clientAuthorizationProperties.getClientId(), clientAuthorizationProperties.getClientSecret());
@@ -35,6 +36,8 @@ public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
         List<Artist> artists = client.getArtists(artistIds);
 
         List<SimplifiedAlbum> arcticMonkeysAlbums = client.getArtistsAlbums("7Ln80lUS6He07XvHI8qqHH");
+
+        List<Track> arcticMonkeysTopTracks = client.getArtistsTopTracks("7Ln80lUS6He07XvHI8qqHH");
 
         System.out.println("App Started");
         System.out.println(clientAuthorizationProperties.getClientId());
