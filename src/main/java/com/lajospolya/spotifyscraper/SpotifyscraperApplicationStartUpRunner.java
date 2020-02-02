efcceaf0d6081471329bf3584c041081e7c06129
LajosPolya <lajospolya94@gmail.com>
2020-02-02 14:37:52 -0500

    testing get artists albums

--- src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
+++ src/main/java/com/lajospolya/spotifyscraper/SpotifyscraperApplicationStartUpRunner.java
@@ -3,6 +3,7 @@ package com.lajospolya.spotifyscraper;
 import com.lajospolya.spotifyapiwrapper.authorization.AuthorizationResponse;
 import com.lajospolya.spotifyapiwrapper.client.SpotifyApiClient;
 import com.lajospolya.spotifyapiwrapper.client.response.Artist;
+import com.lajospolya.spotifyapiwrapper.client.response.SimplifiedAlbum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -20,10 +21,8 @@ public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
     @Override
     public void run(ApplicationArguments args)
     {
-
-        //BQCuC86c2cQHXuNtQrdy6ranMewVMZOQ2DBWev-gQppLmAuC7Ze5nKBMVqPhkJHolexf3jbVapkiW8Zf4CY
         AuthorizationResponse authorizationResponse = new AuthorizationResponse();
-        authorizationResponse.setAccessToken("BQCuC86c2cQHXuNtQrdy6ranMewVMZOQ2DBWev-gQppLmAuC7Ze5nKBMVqPhkJHolexf3jbVapkiW8Zf4CY");
+        authorizationResponse.setAccessToken("BQBLRW_lvFMqpOZGXVVpeeOAtbqsWwKFBW-9EhfeShbAEbbyCeUZSnrFZw70qHuZxuNQ7aXsn4zmN1f5Puk");
         authorizationResponse.setTokenType("Bearer");
         SpotifyApiClient client = new SpotifyApiClient(authorizationResponse);
         //SpotifyApiClient client = SpotifyAuthorizationManager.getAuthorizedApiClient(clientAuthorizationProperties.getClientId(), clientAuthorizationProperties.getClientSecret());
@@ -34,6 +33,8 @@ public class SpotifyscraperApplicationStartUpRunner implements ApplicationRunner
         artistIds.add("7Ln80lUS6He07XvHI8qqHH");
 
         List<Artist> artists = client.getArtists(artistIds);
+
+        List<SimplifiedAlbum> arcticMonkeysAlbums = client.getArtistsAlbums("7Ln80lUS6He07XvHI8qqHH");
 
         System.out.println("App Started");
         System.out.println(clientAuthorizationProperties.getClientId());
